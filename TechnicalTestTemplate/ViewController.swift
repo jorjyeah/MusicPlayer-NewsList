c5ff03226b5b51ece10521fa33ed48ba0657af79
george_kristian <george_kristian@linkaja.id>
2022-05-30 20:19:19 +0700

    Initial Commit

---
+++ TechnicalTestTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechnicalTestTemplate
+//
+//  Created by Wais on 23/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
